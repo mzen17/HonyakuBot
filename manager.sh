605589cbb38f5d0757ec295d3eb9ea142ee4d498
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2024-02-10 21:01:27 +0000

    Auto detect URI in script.

--- manager.sh
+++ manager.sh
@@ -25,9 +25,18 @@ elif [ "$1" == "run" ]; then
     fi
     echo "If the URL does not root to /, enter base URI into second paramater."
     echo "Format for that is /base_uri"
+    CODESPACE_URI="https://${CODESPACE_NAME}-8000.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+    if [ CODESPACE_URI != "https://-8000." ]; then
+        echo "Auto detected codespace URI: ${CODESPACE_URI}"
+    fi
 
     source .venv/bin/activate
-    export BASE_URI=$2
+    if [ "$2" == "" ]; then
+        export BASE_URI=$CODESPACE_URI
+    else
+        export BASE_URI=$2
+    fi
+    echo "Using URI: ${BASE_URI}"
     uvicorn app.web:app --host 0.0.0.0
 else
     echo "Invalid argument"
